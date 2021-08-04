000dda7e8f76562aebf7534308670ca6fdf4a0fa
rohitshantarampatil <rohitpatil41199@gmail.com>
2021-08-04 09:28:17 +0530

    debug:echo statements added

--- sharding/setupMongo2.sh
+++ sharding/setupMongo2.sh
@@ -7,37 +7,32 @@ echo "eth0ip=$eth0ip" > "$destdir"
 
 
 # Setting up mongo config server
-echo "Setting up mongo config server --------------"
-
-echo "docker compose"
+echo "--------------------Setting up mongo config server --------------"
 docker-compose -f config-server/docker-compose.yaml up -d
 
 sleep 5
 # Setting up shard 1
-echo "Setting up shard 1 ----------------"
-echo "docker compose"
+echo "--------------------Setting up shard 1 --------------------"
 docker-compose -f shard1/docker-compose.yaml up -d
 sleep 5
 # Setting up MongoS
-echo "setting up mongos ----------------------------"
-echo "docker-compose mongos"
+echo "--------------------Setting up mongos ----------------------------"
 docker-compose -f mongos/docker-compose.yaml up -d
 sleep 5
 #Setting up Shard2
-echo "Setting up shard 2-----------------"
+echo "--------------------Setting up shard 2-----------------"
 echo "docker compose"
 docker-compose -f shard2/docker-compose.yaml up -d
 
 
 sleep 5
-
-echo "connect to config"
+echo "------------------connect to config ------------------"
 mongo mongodb://"$eth0ip":27019 scripts/initiateConfigServer.js
 
 
 sleep 10
 
-echo "initiateShard1"
+echo "-------------------initiateShard1----------------------"
 mongo mongodb://"$eth0ip":27020 scripts/initiateReplicaSetForShard1.js
 
 
@@ -45,21 +40,19 @@ mongo mongodb://"$eth0ip":27020 scripts/initiateReplicaSetForShard1.js
 sleep 10
 
 #adding Shard1 in mongos
-echo "Adding shard1 -------------------------"
-echo "mongo addshards"
+echo "-------------------Adding shard1 -------------------------"
 mongo mongodb://"$eth0ip":27017 scripts/addShard1.js
 sleep 10
 
 
 sleep 10
-echo "initiateShard2"
+echo "--------------------initiateShard2-------------------------"
 mongo mongodb://"$eth0ip":50004 scripts/initiateReplicaSetForShard2.js
 
 
 
 #adding shard2
-echo "Adding shard2 --------------------"
-echo "mongo addshards"
+echo "---------------------Adding shard2 --------------------"
 mongo mongodb://"$eth0ip":27017 scripts/addShard2.js
 
 
