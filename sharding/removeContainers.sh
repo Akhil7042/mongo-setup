bb57119ed61075af3a68232e2dc01434230e2474
rohitshantarampatil <rohitpatil41199@gmail.com>
2021-08-04 10:22:22 +0530

    bug : removed bug for removeContainers.sh

--- sharding/removeContainers.sh
+++ sharding/removeContainers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker compose -f config-server/docker-compose.yaml rm -fsv
-docker compose -f mongos/docker-compose.yaml rm -fsv
-docker compose -f shard1/docker-compose.yaml rm -fsv
-docker compose -f shard2/docker-compose.yaml rm -fsv
+docker-compose -f config-server/docker-compose.yaml rm -fsv
+docker-compose -f mongos/docker-compose.yaml rm -fsv
+docker-compose -f shard1/docker-compose.yaml rm -fsv
+docker-compose -f shard2/docker-compose.yaml rm -fsv
