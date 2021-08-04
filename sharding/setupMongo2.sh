8c29e41fa779b307e53e69dd1736a312b3dd9865
rohitshantarampatil <rohitpatil41199@gmail.com>
2021-08-04 12:56:58 +0530

    time increased

--- sharding/setupMongo2.sh
+++ sharding/setupMongo2.sh
@@ -11,51 +11,64 @@ export eth0ip="$eth0ip"
 echo "--------------------Setting up mongo config server --------------"
 docker-compose -f config-server/docker-compose.yaml up -d
 
-sleep 20
+sleep 30
 # Setting up shard 1
 echo "--------------------Setting up shard 1 --------------------"
 docker-compose -f shard1/docker-compose.yaml up -d
-sleep 5
+sleep 30
 # Setting up MongoS
 echo "--------------------Setting up mongos ----------------------------"
 docker-compose -f mongos/docker-compose.yaml up -d
-sleep 5
+sleep 30
 #Setting up Shard2
 echo "--------------------Setting up shard 2-----------------"
 echo "docker compose"
 docker-compose -f shard2/docker-compose.yaml up -d
 
 
-sleep 5
+sleep 30
 echo "------------------connect to config ------------------"
+docker exec cfgsvr mongo
+sleep 5
+echo "------------------run config scripts ------------------"
 docker exec cfgsvr mongo < scripts/initiateConfigServer.js
 
 
-sleep 10
-
+sleep 40
+echo "------------------connect to shard1 ------------------"
+docker exec shard1svr1 mongo
+sleep 5 
 echo "-------------------initiateShard1----------------------"
 docker exec shard1svr1 mongo  < scripts/initiateReplicaSetForShard1.js
 
 
 
-sleep 10
+sleep 40
 
 #adding Shard1 in mongos
+echo "------------------connect to mongos ------------------"
+docker exec mongos mongo
+sleep 5
 echo "-------------------Adding shard1 -------------------------"
 docker exec mongos mongo < scripts/addShard1.js
-sleep 10
+sleep 40
 
 
+echo "--------------------connect to Shard2-------------------------"
+docker exec shard2svr1 mongo
 sleep 10
 echo "--------------------initiateShard2-------------------------"
 docker exec shard2svr1 mongo < scripts/initiateReplicaSetForShard2.js
+sleep 40
 
 
-
+echo "------------------connect to mongos ------------------"
+docker exec mongos mongo
+sleep 10
 #adding shard2
 echo "---------------------Adding shard2 --------------------"
 docker exec mongos mongo < scripts/addShard2.js
-
+sleep 40
 
 
 # echo "enableShardsOnDB"
