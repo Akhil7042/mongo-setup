aede53133b4c0752b29b912c91a7152cf3034281
rohitshantarampatil <rohitpatil41199@gmail.com>
2021-08-04 10:55:54 +0530

    fix : docker bug fixed

--- sharding/setupMongo2.sh
+++ sharding/setupMongo2.sh
@@ -2,8 +2,9 @@
 destdir=./mongos/.env
 
 eth0ip=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-# eth0ip="192.168.1.7"
+# eth0ip="192.168.43.216"
 echo "eth0ip=$eth0ip" > "$destdir"
+export eth0ip="$eth0ip"
 
 
 # Setting up mongo config server
@@ -27,13 +28,13 @@ docker-compose -f shard2/docker-compose.yaml up -d
 
 sleep 5
 echo "------------------connect to config ------------------"
-mongo mongodb://"$eth0ip":27019 scripts/initiateConfigServer.js
+docker exec -it cfgsvr mongo mongodb://"$eth0ip":27019 scripts/initiateConfigServer.js
 
 
 sleep 10
 
 echo "-------------------initiateShard1----------------------"
-mongo mongodb://"$eth0ip":27020 scripts/initiateReplicaSetForShard1.js
+docker exec -it shard1svr1 mongo mongodb://"$eth0ip":27020 scripts/initiateReplicaSetForShard1.js
 
 
 
@@ -41,24 +42,24 @@ sleep 10
 
 #adding Shard1 in mongos
 echo "-------------------Adding shard1 -------------------------"
-mongo mongodb://"$eth0ip":27017 scripts/addShard1.js
+docker exec -it mongos mongo mongodb://"$eth0ip":27017 scripts/addShard1.js
 sleep 10
 
 
 sleep 10
 echo "--------------------initiateShard2-------------------------"
-mongo mongodb://"$eth0ip":50004 scripts/initiateReplicaSetForShard2.js
+docker exec -it shard2svr1 mongo mongodb://"$eth0ip":50004 scripts/initiateReplicaSetForShard2.js
 
 
 
 #adding shard2
 echo "---------------------Adding shard2 --------------------"
-mongo mongodb://"$eth0ip":27017 scripts/addShard2.js
+docker exec -it mongos mongo mongodb://"$eth0ip":27017 scripts/addShard2.js
 
 
 
 # echo "enableShardsOnDB"
-# mongo mongodb://"$eth0ip":27017 scripts/enableShardsOnDB.js
+docker exec -it mongos mongo mongodb://"$eth0ip":27017 scripts/enableShardsOnDB.js
 
 
 
